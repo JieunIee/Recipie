f45c8e38c5ba676a4550a7ab4285bc90cbcb02a8
Ieejueun <zziya2006@gmail.com>
2024-06-10 19:24:45 +0900

    [Feat] 스플래시, 앱아이콘 구현

--- App.tsx
+++ App.tsx
@@ -5,11 +5,17 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/Router.tsx';
+import SplashScreen from "react-native-splash-screen";
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  });
   return (
     <NavigationContainer>
       <Router />
